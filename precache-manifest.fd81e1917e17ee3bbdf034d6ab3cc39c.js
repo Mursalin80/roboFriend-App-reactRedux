992648f793bdc2fafafa16d8f13d114803b592cf
mursalin80 <mursalin80@hotmail.com>
2019-12-27 11:30:58 +0500

    Updates

---
+++ precache-manifest.fd81e1917e17ee3bbdf034d6ab3cc39c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "483a4741ce7576703d9a3ec150a503bf",
+    "url": "/Mursalin80/roboFriend-App-reactRedux/index.html"
+  },
+  {
+    "revision": "f051b1bb7c8858f42eee",
+    "url": "/Mursalin80/roboFriend-App-reactRedux/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "7c3a9e62df371683b16d",
+    "url": "/Mursalin80/roboFriend-App-reactRedux/static/css/main.6b300338.chunk.css"
+  },
+  {
+    "revision": "f051b1bb7c8858f42eee",
+    "url": "/Mursalin80/roboFriend-App-reactRedux/static/js/2.ca777740.chunk.js"
+  },
+  {
+    "revision": "7c3a9e62df371683b16d",
+    "url": "/Mursalin80/roboFriend-App-reactRedux/static/js/main.357dc0bd.chunk.js"
+  },
+  {
+    "revision": "6fe535747d45ea12bb49",
+    "url": "/Mursalin80/roboFriend-App-reactRedux/static/js/runtime-main.f3ec7e88.js"
+  }
+]);
