7b5a8afe96c51b2aea25ac86dcf016b90e965d4a
mursalin80 <mursalin80@hotmail.com>
2019-12-28 12:04:51 +0500

    hi

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Cart = ({ id, name, email }) => (
   <div className="bg-light-green dib br3 pa3 ma2 grow">
-    <img src={`https://robohash.org/${id}?200*300`} alt="robo" />
+    <img src={`https://robohash.org/${id}?size=200*200`} alt="robo" />
     <h2>{name}</h2>
     <p>Email:{email}</p>
   </div>
--- src/components/Search.js
+++ src/components/Search.js
@@ -3,6 +3,7 @@ import React from "react";
 const Search = ({ searchChange }) => (
   <div className="pa2">
     <input
+      aria-lable="Search Robots"
       className="pa3 ba b--green bg-lightest-blue"
       type="text"
       placeholder="Search Rebort"
